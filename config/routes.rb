ff3f6c256175fcba52e1f41fcb36d0a42b7ac215
zhaoli <seseva57@gmail.com>
2017-02-24 17:34:35 +0800

    product categorize

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
@@ -21,6 +22,7 @@ Rails.application.routes.draw do
     member do
       post :add_to_cart
     end
+    resources :comments
   end
 
   resources :carts do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215142051) do
+ActiveRecord::Schema.define(version: 20170224082350) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20170215142051) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "product_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -55,9 +65,11 @@ ActiveRecord::Schema.define(version: 20170215142051) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "image"
+    t.string   "categories"
+    t.integer  "stars_number"
   end
 
   create_table "users", force: :cascade do |t|
