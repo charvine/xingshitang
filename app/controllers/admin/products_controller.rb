1c5ea32825ab047f4229cd3df90187ec192d76c6
charvine <crossovermeister@gmail.com>
2017-03-02 22:00:21 +0800

    Add category function

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,11 +4,17 @@ class Admin::ProductsController < ApplicationController
   layout "admin"
 
   def index
-    @products=Product.all
+    if params[:category].blank?
+	   @products=Product.all
+    else
+	     @category_id = Category.find_by(name: params[:category]).id
+	    @products= Product.where(:category_id => @category_id).order(“created_at DESC”)
+    end
   end
 
   def new
     @product=Product.new
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def show
@@ -17,10 +23,12 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product=Product.find(params[:id])
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def create
     @product=Product.new(product_params)
+    @product.category_id = params[:category_id]
      if @product.save
        redirect_to admin_products_path
      else
@@ -30,6 +38,7 @@ class Admin::ProductsController < ApplicationController
 
   def update
     @product=Product.find(params[:id])
+    @product.category_id = params[:category_id]
       if @product.update(product_params)
         redirect_to admin_products_path
       else
@@ -46,6 +55,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :image, :categories)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :categories, :category_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225034536) do
+ActiveRecord::Schema.define(version: 20170302121104) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170225034536) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -77,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170225034536) do
     t.string   "image"
     t.string   "categories"
     t.integer  "stars_number"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
